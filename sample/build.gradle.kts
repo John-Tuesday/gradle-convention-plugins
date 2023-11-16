cf134aa0cebc1a30361866a64ce671395faccd01
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-15 20:43:17 -0600

    Apply kotlin multiplatform to :sample module

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
+    alias(libs.plugins.kotlin.multiplatform)
     id("io.github.john-tuesday.maven-publish-assist")
 }
+
+kotlin {
+    applyDefaultHierarchyTemplate()
+    linuxX64()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
