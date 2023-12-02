2f3788130c59351a75efa7be70fe7638ea71e57e
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-02 03:16:57 -0600

    Fix non-working basic apply test

--- plugin/maven-publish-assist/src/functionalTest/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginFunctionalTest.kt
+++ plugin/maven-publish-assist/src/functionalTest/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginFunctionalTest.kt
@@ -1,34 +1,41 @@
 package io.github.john.tuesday.plugins
 
+import io.github.john.tuesday.plugins.helper.PgpInMemoryKeys
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import kotlin.test.Test
-import kotlin.test.assertTrue
 
 class MavenPublishAssistPluginFunctionalTest {
 
     @field:TempDir
     lateinit var projectDir: File
 
-    private val buildFile by lazy { projectDir.resolve("build.gradle") }
-    private val settingsFile by lazy { projectDir.resolve("settings.gradle") }
+    private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
+    private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
 
     @Test
     fun `maven quick`() {
         settingsFile.writeText("")
-        buildFile.writeText("""
+        buildFile.writeText(
+            """
             plugins {
                 id("io.github.john-tuesday.maven-publish-assist")
             }
-        """.trimIndent())
+        """.trimIndent()
+        )
 
         val runner = GradleRunner.create()
-        runner.forwardOutput()
-        runner.withPluginClasspath()
-        runner.withProjectDir(projectDir)
+            .forwardOutput()
+            .withPluginClasspath()
+            .withProjectDir(projectDir)
+            .withEnvironment(
+                mapOf(
+                    PgpInMemoryKeys.SECRET_KEY_ENVIRONMENT to "",
+                    PgpInMemoryKeys.PASSWORD_ENVIRONMENT to "",
+                )
+            )
+            .withArguments(":check")
         val result = runner.build()
-
-        println(result.output)
     }
 }
