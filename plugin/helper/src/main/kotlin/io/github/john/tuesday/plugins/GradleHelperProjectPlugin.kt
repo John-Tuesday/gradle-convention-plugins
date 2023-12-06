9a6e4a278755d51575ab9b537920f3f4b40c3d4f
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-06 05:47:15 -0600

    Add documentation

--- plugin/helper/src/main/kotlin/io/github/john/tuesday/plugins/GradleHelperProjectPlugin.kt
+++ plugin/helper/src/main/kotlin/io/github/john/tuesday/plugins/GradleHelperProjectPlugin.kt
@@ -3,6 +3,9 @@ package io.github.john.tuesday.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * No-op plugin meant to add the helper api to the classpath in an IDE friendly way
+ */
 public class GradleHelperProjectPlugin : Plugin<Project> {
     override fun apply(target: Project) {}
 }
--- plugin/helper/src/main/kotlin/io/github/john/tuesday/plugins/GradleHelperSettingsPlugin.kt
+++ plugin/helper/src/main/kotlin/io/github/john/tuesday/plugins/GradleHelperSettingsPlugin.kt
@@ -3,6 +3,9 @@ package io.github.john.tuesday.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 
+/**
+ * No-op plugin meant to add the helper api to the classpath in an IDE friendly way
+ */
 public class GradleHelperSettingsPlugin : Plugin<Settings> {
     override fun apply(target: Settings) {}
 }
