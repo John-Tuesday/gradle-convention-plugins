9d018e8c113be3bbaeb2e48fd5430394017413ef
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-06 05:21:55 -0600

    Add tests for include and exclude

--- plugin/maven-publish-assist/src/functionalTest/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginFunctionalTest.kt
+++ plugin/maven-publish-assist/src/functionalTest/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginFunctionalTest.kt
@@ -2,9 +2,13 @@ package io.github.john.tuesday.plugins
 
 import io.github.john.tuesday.plugins.helper.PgpInMemoryKeys
 import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class MavenPublishAssistPluginFunctionalTest {
 
@@ -13,6 +17,37 @@ class MavenPublishAssistPluginFunctionalTest {
 
     private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
     private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
+
+    private val kotlinMultiplatformBuildFileString =
+        """
+            plugins {
+                kotlin("multiplatform") version "1.9.21"
+                id("io.github.john-tuesday.maven-publish-assist")
+            }
+            
+            kotlin {
+                jvm()
+                linuxX64()
+            }
+        """.trimIndent()
+    private val kotlinMultiplatformSettingsFileString =
+        """
+            dependencyResolutionManagement {
+                repositories {
+                    mavenCentral()
+                }
+            }
+        """.trimIndent()
+
+    private fun kmmRunner(): GradleRunner {
+        settingsFile.writeText(kotlinMultiplatformSettingsFileString)
+        buildFile.writeText(kotlinMultiplatformBuildFileString)
+
+        return GradleRunner.create()
+            .forwardOutput()
+            .withPluginClasspath()
+            .withProjectDir(projectDir)
+    }
 
     @Test
     fun `maven quick`() {
@@ -37,5 +72,71 @@ class MavenPublishAssistPluginFunctionalTest {
             )
             .withArguments(":check")
         val result = runner.build()
+    }
+
+    @Test
+    fun `exclude targets`() {
+        val runner = kmmRunner()
+            .withEnvironment(
+                mapOf(
+                    PgpInMemoryKeys.SECRET_KEY_ENVIRONMENT to "",
+                    PgpInMemoryKeys.PASSWORD_ENVIRONMENT to "",
+                    FilterTargetKeys.EXCLUDE_ENVIRONMENT to "(Jvm)|(LinuxX64)",
+                )
+            )
+            .withArguments(":publishJvmPublicationToMavenLocal")
+        val result = runner.build()
+
+        val signJvmTask = result.task(":signJvmPublication")
+        assertNotNull(signJvmTask)
+        assertEquals(TaskOutcome.SKIPPED, signJvmTask.outcome)
+
+        val publishJvmTask = result.task(":publishJvmPublicationToMavenLocal")
+        assertNotNull(publishJvmTask)
+        assertEquals(TaskOutcome.SKIPPED, publishJvmTask.outcome)
+
+        val result2 = runner
+            .withArguments(":publishKotlinMultiplatformPublicationToMavenLocal", "--continue", "--exclude-task", ":signKotlinMultiplatformPublication")
+            .buildAndFail()
+
+        val signKmpTask = result2.task(":signKotlinMultiplatformPublication")
+        assertNull(signKmpTask)
+
+        val publishKmpTask = result2.task(":publishKotlinMultiplatformPublicationToMavenLocal")
+        assertNotNull(publishKmpTask)
+        assertEquals(TaskOutcome.FAILED, publishKmpTask.outcome)
+    }
+
+    @Test
+    fun `include targets`() {
+        val runner = kmmRunner()
+            .withEnvironment(
+                mapOf(
+                    PgpInMemoryKeys.SECRET_KEY_ENVIRONMENT to "",
+                    PgpInMemoryKeys.PASSWORD_ENVIRONMENT to "",
+                    FilterTargetKeys.INCLUDE_ENVIRONMENT to "(Jvm)|(LinuxX64)",
+                )
+            )
+            .withArguments(":publishJvmPublicationToMavenLocal", "--continue", "--exclude-task", ":signJvmPublication")
+        val result = runner.buildAndFail()
+
+        val signJvmTask = result.task(":signJvmPublication")
+        assertNull(signJvmTask)
+
+        val publishJvmTask = result.task(":publishJvmPublicationToMavenLocal")
+        assertNotNull(publishJvmTask)
+        assertEquals(TaskOutcome.FAILED, publishJvmTask.outcome)
+
+        val result2 = runner
+            .withArguments(":publishKotlinMultiplatformPublicationToMavenLocal")
+            .build()
+
+        val signKmpTask = result2.task(":signKotlinMultiplatformPublication")
+        assertNotNull(signKmpTask)
+        assertEquals(TaskOutcome.SKIPPED, signKmpTask.outcome)
+
+        val publishKmpTask = result2.task(":publishKotlinMultiplatformPublicationToMavenLocal")
+        assertNotNull(publishKmpTask)
+        assertEquals(TaskOutcome.SKIPPED, publishKmpTask.outcome)
     }
 }
--- plugin/maven-publish-assist/src/test/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginUnitTest.kt
+++ plugin/maven-publish-assist/src/test/kotlin/io/github/john/tuesday/plugins/MavenPublishAssistPluginUnitTest.kt
@@ -1,20 +1,36 @@
 package io.github.john.tuesday.plugins
 
 import io.github.john.tuesday.plugins.helper.PgpInMemoryKeys
+import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.signing.SigningExtension
 import org.gradle.testfixtures.ProjectBuilder
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class MavenPublishAssistPluginUnitTest {
+    private val defaultProperties = mapOf (
+        PgpInMemoryKeys.SECRET_KEY_PROPERTY to "",
+        PgpInMemoryKeys.PASSWORD_PROPERTY to "",
+ )
+
+    private fun defaultProject(): Project {
+        val project = ProjectBuilder.builder().build()
+        for ((key, value) in defaultProperties)
+            project.extraProperties.set(key, value)
+        return project
+    }
+
     @Test
     fun `maven quick publish plugin`() {
-        System.setProperty(PgpInMemoryKeys.SECRET_KEY_PROPERTY, "")
-        System.setProperty(PgpInMemoryKeys.PASSWORD_PROPERTY, "")
-        val project = ProjectBuilder.builder().build()
-        project.plugins.apply("io.github.john-tuesday.maven-publish-assist")
+        val project = defaultProject()
+        project.pluginManager.apply(MavenPublishAssistPlugin::class)
 
         val publishingExtension = project.extensions.findByType<PublishingExtension>()
         assertNotNull(publishingExtension)
@@ -24,5 +40,72 @@ class MavenPublishAssistPluginUnitTest {
 
         val publishTask = project.tasks.named("publishToMavenLocal")
         assertNotNull(publishTask.orNull)
+    }
+
+    @Test
+    fun `exclude targets`() {
+        val project = defaultProject()
+        project.extraProperties.set(FilterTargetKeys.EXCLUDE_PROPERTY, "(Jvm)|(KotlinMultiplatform)")
+        project.pluginManager.apply(KotlinMultiplatformPluginWrapper::class)
+        project.pluginManager.apply(MavenPublishAssistPlugin::class)
+
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            linuxX64()
+            jvm()
+        }
+
+        assertFalse {
+            val jvmPublishTask by project.tasks.named("publishJvmPublicationToMavenLocal")
+            jvmPublishTask.enabled
+        }
+        assertTrue {
+            val publishLinuxX64PublicationToMavenLocal by project.tasks.getting
+            publishLinuxX64PublicationToMavenLocal.enabled
+        }
+    }
+
+    @Test
+    fun `include targets`() {
+        val project = defaultProject()
+        project.extraProperties.set(FilterTargetKeys.INCLUDE_PROPERTY, "(Jvm)|(KotlinMultiplatform)")
+        project.pluginManager.apply(KotlinMultiplatformPluginWrapper::class)
+        project.pluginManager.apply(MavenPublishAssistPlugin::class)
+
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            linuxX64()
+            jvm()
+        }
+
+        assertTrue {
+            val jvmPublishTask by project.tasks.named("publishJvmPublicationToMavenLocal")
+            jvmPublishTask.enabled
+        }
+        assertFalse {
+            val publishLinuxX64PublicationToMavenLocal by project.tasks.getting
+            publishLinuxX64PublicationToMavenLocal.enabled
+        }
+    }
+
+    @Test
+    fun `include and exclude targets prioritizes exclude over include`() {
+        val project = defaultProject()
+        project.extraProperties.set(FilterTargetKeys.EXCLUDE_PROPERTY, "(Jvm)|(KotlinMultiplatform)")
+        project.extraProperties.set(FilterTargetKeys.INCLUDE_PROPERTY, "(Jvm)|(KotlinMultiplatform)")
+        project.pluginManager.apply(KotlinMultiplatformPluginWrapper::class)
+        project.pluginManager.apply(MavenPublishAssistPlugin::class)
+
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            linuxX64()
+            jvm()
+        }
+
+        assertFalse {
+            val jvmPublishTask by project.tasks.named("publishJvmPublicationToMavenLocal")
+            jvmPublishTask.enabled
+        }
+        assertFalse {
+            val publishLinuxX64PublicationToMavenLocal by project.tasks.getting
+            publishLinuxX64PublicationToMavenLocal.enabled
+        }
     }
 }
